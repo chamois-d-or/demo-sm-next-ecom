aa0cc2aab4d1f3cfe479da16311e763827c99e29
chamois-d-or <89452979+chamois-d-or@users.noreply.github.com>
2021-12-13 12:11:51 +0100

    add CategPreviewScrolling slice slice

--- .slicemachine/assets/slices/marketing/HeaderSectionSimpleCentered/index.stories.js
+++ .slicemachine/assets/slices/marketing/HeaderSectionSimpleCentered/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"header_section_simple_centered","items":[],"primary":{"title":[{"type":"heading1","text":"Exploit one-to-one content","spans":[]}],"description":[{"type":"paragraph","text":"Nisi fugiat quis velit sunt sint dolor enim id tempor id irure veniam. Pariatur ut veniam ullamco eu consequat.","spans":[]}]},"id":"_DefaultSlice"}} />
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"header_section_simple_centered","items":[],"primary":{"title":[{"type":"heading1","text":"Maximize B2B channels","spans":[]}],"description":[{"type":"paragraph","text":"Consequat aliquip anim commodo do voluptate id ea ullamco adipisicing aute proident et.","spans":[]}]},"id":"_DefaultSlice"}} />
 _DefaultSlice.storyName = 'Default slice'
