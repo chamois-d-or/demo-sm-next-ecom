96c522c3b4d5aae29a6707f9bdf7ea8fdba79987
chamois-d-or <89452979+chamois-d-or@users.noreply.github.com>
2022-01-20 12:37:21 +0100

    add form slice to hp

--- .slicemachine/assets/slices/marketing/HeaderSectionSimpleCentered/index.stories.js
+++ .slicemachine/assets/slices/marketing/HeaderSectionSimpleCentered/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"header_section_simple_centered","items":[],"primary":{"title":[{"type":"heading1","text":"Enable back-end mindshare","spans":[]}],"description":[{"type":"paragraph","text":"Commodo cupidatat esse deserunt sunt cupidatat. Incididunt id magna cupidatat sit non officia fugiat. Irure enim laborum elit aute eu ipsum ullamco eiusmod in incididunt fugiat sint dolore.","spans":[]}]},"id":"_DefaultSlice"}} />
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"header_section_simple_centered","items":[],"primary":{"title":[{"type":"heading1","text":"Enable sticky interfaces","spans":[]}],"description":[{"type":"paragraph","text":"Sint sunt ad minim ea ullamco reprehenderit. Officia duis quis anim esse eiusmod anim enim adipisicing incididunt aute.","spans":[]}]},"id":"_DefaultSlice"}} />
 _DefaultSlice.storyName = 'Default slice'
